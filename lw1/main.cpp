c600750bc4332f215e268353a64f0f75f434a0ea
Pavel Ataykin <pavel.ataykin@gmail.com>
2018-09-10 20:24:14 +0300

    Залил 1-ю лабу.

---
+++ lw1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+bool isPalindorm(std::string word) {
+    for (int i = 0; i < std::floor(word.length() / 2); ++i) {
+        if (word[i] != word[word.length() - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isEmptyFile(std::ifstream &pFile) {
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+int main() {
+    std::ifstream file("../input.txt");
+
+    if (!file.is_open()) {
+        return 1;
+    }
+    if (isEmptyFile(file)) {
+        std::cout << "File is empty." << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    std::vector<std::string> list;
+    unsigned long long int maxLength = 0;
+
+    while (!file.eof()) {
+        file >> word;
+        if (isPalindorm(word)) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+                list.clear();
+            }
+            if (word.length() == maxLength) {
+                list.push_back(word);
+            }
+        }
+    }
+
+    if (list.empty()) {
+        std::cout << "Palindrom is not defined." << std::endl;
+    } else {
+        for (std::string elem:list) { std::cout << elem << std::endl; }
+    }
+
+    return 0;
+}

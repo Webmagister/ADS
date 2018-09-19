275384f859ea602422de4d83c5607a6d731623e9
pavelataykin <pavel.ataykin@gmail.com>
2018-09-19 10:11:46 +0300

    Поправил форматирование.

--- lw2/main.cpp
+++ lw2/main.cpp
@@ -13,39 +13,47 @@
 #include <map>
 #include <vector>
 
-struct Stack {
-
+struct Stack
+{
     std::vector<char> list;
 
-    char top() {
+    char top()
+    {
         return list.back();
     }
 
-    void pop() {
+    void pop()
+    {
         list.pop_back();
     }
 
-    void push(char symbol) {
+    void push(char symbol)
+    {
         list.push_back(symbol);
     }
 
-    bool empty() {
+    bool empty()
+    {
         return list.empty();
     }
 };
 
-bool isEmptyFile(std::ifstream &pFile) {
+bool isEmptyFile(std::ifstream &pFile)
+{
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         std::cout << "There are not enough files." << std::endl;
         return 1;
     }
 
-    if (static_cast<std::string>(argv[1]) == static_cast<std::string>(argv[2])) {
+    if (static_cast<std::string>(argv[1]) == static_cast<std::string>(argv[2]))
+    {
         std::cout << "Files is equal." << std::endl;
         return 1;
     }
@@ -53,18 +61,20 @@ int main(int argc, char *argv[]) {
     std::ifstream inputFile(argv[1]);
     std::ofstream outputFile(argv[2]);
 
-    if (!inputFile.is_open() || !outputFile.is_open()) {
+    if (!inputFile.is_open() || !outputFile.is_open())
+    {
         std::cout << "File does not open." << std::endl;
         return 1;
     }
 
-    if (isEmptyFile(inputFile)) {
+    if (isEmptyFile(inputFile))
+    {
         outputFile << "File is empty." << std::endl;
         inputFile.close();
         outputFile.close();
         return 1;
     }
-    
+
     Stack stack;
 
     const std::map<char, int> arithmeticSings = {
@@ -76,27 +86,35 @@ int main(int argc, char *argv[]) {
             {'^', 3}
     };
 
-    while (!inputFile.eof()) {
+    while (!inputFile.eof())
+    {
         char symbol = inputFile.get();
-        if (symbol != ' ' && symbol != std::ifstream::traits_type::eof()) {
-            if (isdigit(symbol)) {
+        if (symbol != ' ' && symbol != std::ifstream::traits_type::eof())
+        {
+            if (isdigit(symbol))
+            {
                 outputFile << symbol;
             }
-            if (arithmeticSings.find(symbol) != arithmeticSings.end()) {
+            if (arithmeticSings.find(symbol) != arithmeticSings.end())
+            {
                 outputFile << ' ';
                 while (!stack.empty() &&
                        arithmeticSings.find(stack.top())->second >= arithmeticSings.find(symbol)->second
-                       && symbol != '(') {
+                       && symbol != '(')
+                {
                     outputFile << stack.top();
                     stack.pop();
                 }
                 stack.push(symbol);
             }
-            if (symbol == ')') {
+            if (symbol == ')')
+            {
                 char symbolInStack = ')';
-                while (symbolInStack != '(') {
+                while (symbolInStack != '(')
+                {
                     symbolInStack = stack.top();
-                    if (symbolInStack != '(') {
+                    if (symbolInStack != '(')
+                    {
                         outputFile << symbolInStack;
                     }
                     stack.pop();
@@ -105,8 +123,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (!stack.empty()) {
-        while (!stack.empty()) {
+    if (!stack.empty())
+    {
+        while (!stack.empty())
+        {
             outputFile << stack.top();
             stack.pop();
         }

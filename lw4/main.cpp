735e4c2347767e08bc23520b596536a5aa862944
pavelataykin <pavel.ataykin@gmail.com>
2018-11-25 18:44:25 +0300

    Реализовал обход в ширину, вывод всех возможных путей из 1 в последний аэропорт.

--- lw4/main.cpp
+++ lw4/main.cpp
@@ -13,13 +13,47 @@
 #include <vector>
 #include <string>
 
-struct Tree
+struct Graph
 {
     std::string name = "";
-    std::string weightStr = "";
+    std::string flightCode = "";
 
-    std::vector<Tree> children;
+    std::vector<Graph *> linked;
 };
+
+bool alreadyInList(std::string currChildName, std::vector<std::string> currPath)
+{
+    for (int i = 0; i < currPath.size(); ++i)
+    {
+        if (currChildName == currPath[i])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void bfs(std::vector<std::vector<std::string>> &allPath, Graph &currChild, std::string &start,
+         std::string &end, std::vector<std::string> currPath)
+{
+    currPath.push_back(currChild.name);
+
+    for (Graph *child : currChild.linked)
+    {
+        if (child->name == end)
+        {
+            currPath.push_back(child->name);
+            allPath.push_back(currPath);
+
+            continue;
+        }
+        if (!alreadyInList(child->name, currPath))
+        {
+            bfs(allPath, *child, start, end, currPath);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -36,12 +70,58 @@ int main(int argc, char *argv[])
     }
 
     std::ifstream inputFile(argv[1]);
+    std::ifstream dictionaryFile("dictionary.txt");
     std::ofstream outputFile(argv[2]);
 
     if (!inputFile.is_open() || !outputFile.is_open())
     {
         std::cout << "File does not open." << std::endl;
         return 1;
+    }
+
+    std::vector<Graph> listOfTops;
+
+    while (!inputFile.eof())
+    {
+        Graph root;
+        std::string str;
+        inputFile >> str;
+        root.name = str;
+        if (!str.empty())
+        {
+            listOfTops.push_back(root);
+        }
+    }
+
+    while (!dictionaryFile.eof())
+    {
+        std::string str;
+        std::string str2;
+        dictionaryFile >> str;
+        dictionaryFile >> str2;
+        listOfTops[std::stoi(str) - 1].linked.push_back(&listOfTops[std::stoi(str2) - 1]);
+    }
+
+    std::vector<std::vector<std::string>> allPath;
+    std::string start = listOfTops.front().name;
+    std::string end = listOfTops.back().name;
+
+    std::vector<std::string> curPath;
+    bfs(allPath, listOfTops.front(), start, end, curPath);
+
+//    for (Graph top : listOfTops)
+//    {
+//        std::cout << top.name << " " << top.linked.size() << std::endl;
+//    }
+
+    for (std::vector<std::string> &item : allPath)
+    {
+        for (int i = 0; i < item.size(); ++i)
+        {
+            std::cout << item[i] << " ";
+        }
+
+        std::cout << std::endl;
     }
 
     inputFile.close();

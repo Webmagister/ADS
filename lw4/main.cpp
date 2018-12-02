3f0708ec52fd78eb9ab0cbf29f5dddb300aeb5c6
pavelataykin <pavel.ataykin@gmail.com>
2018-12-02 16:16:06 +0300

    Добавил обработку нескольких рейсов из аэропорта.

--- lw4/main.cpp
+++ lw4/main.cpp
@@ -17,10 +17,16 @@
 struct Graph
 {
     std::string name = "";
-    std::string flightCode = "";
 
     std::vector<Graph *> linked;
+
+    std::map<std::string, std::vector<std::string>> children;
 };
+
+bool isNodeInMap(std::map<std::string, std::vector<std::string>> map, std::string nodeName)
+{
+    return map.find(nodeName) != map.end();
+}
 
 void printCurrPath(std::vector<std::string> currPath)
 {
@@ -44,17 +50,39 @@ bool alreadyInList(std::string currChildName, std::vector<std::string> currPath)
     return false;
 }
 
+bool alreadyInVector(std::string currChildName, std::vector<Graph *> root)
+{
+    for (int i = 0; i < root.size(); ++i)
+    {
+        if (currChildName == root[i]->name)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 void bfs(std::vector<std::vector<std::string>> &allPath, Graph &currChild, std::string &start,
          std::string &end, std::vector<std::string> currPath)
 {
     currPath.push_back(currChild.name);
-    printCurrPath(currPath);
-    std::cout << "------" << std::endl;
+//    printCurrPath(currPath);
+//    std::cout << "--------------------------" << std::endl;
 
     for (Graph *child : currChild.linked)
     {
         if (child->name == end)
         {
+            if (isNodeInMap(currChild.children, child->name))
+            {
+                std::string flightCodes;
+                for (int i = 0; i < currChild.children.at(child->name).size(); ++i)
+                {
+                    flightCodes += currChild.children.at(child->name)[i];
+                }
+                currPath.push_back(flightCodes);
+            }
             currPath.push_back(child->name);
             allPath.push_back(currPath);
 
@@ -62,7 +90,20 @@ void bfs(std::vector<std::vector<std::string>> &allPath, Graph &currChild, std::
         }
         if (!alreadyInList(child->name, currPath))
         {
-            bfs(allPath, *child, start, end, currPath);
+            std::vector<std::string> tempPath;
+            tempPath = currPath;
+
+            if (isNodeInMap(currChild.children, child->name))
+            {
+                std::string flightCodes;
+                for (int i = 0; i < currChild.children.at(child->name).size(); ++i)
+                {
+                    flightCodes += currChild.children.at(child->name)[i];
+                }
+                tempPath.push_back(flightCodes);
+            }
+
+            bfs(allPath, *child, start, end, tempPath);
         }
     }
 }
@@ -82,7 +123,7 @@ int main(int argc, char *argv[])
     }
 
     std::ifstream inputFile(argv[1]);
-    std::ifstream dictionaryFile("dictionary1.txt");
+    std::ifstream dictionaryFile("dictionary.txt");
     std::ofstream outputFile(argv[2]);
 
     if (!inputFile.is_open() || !outputFile.is_open())
@@ -109,11 +150,33 @@ int main(int argc, char *argv[])
     {
         std::string str;
         std::string str2;
+        std::string flightCode;
 
         dictionaryFile >> str;
         dictionaryFile >> str2;
+        getline(dictionaryFile, flightCode);
 
-        listOfTops[std::stoi(str) - 1].linked.push_back(&listOfTops[std::stoi(str2) - 1]);
+        if (!alreadyInVector(listOfTops[std::stoi(str2) - 1].name, listOfTops[std::stoi(str) - 1].linked))
+        {
+            listOfTops[std::stoi(str) - 1].linked.push_back(&listOfTops[std::stoi(str2) - 1]);
+        }
+
+        std::string name = listOfTops[std::stoi(str2) - 1].name;
+        if (!flightCode.empty())
+        {
+            Graph *root = &listOfTops[std::stoi(str) - 1];
+
+            if (!isNodeInMap(root->children, name))
+            {
+                std::vector<std::string> flightCodes;
+                flightCodes.push_back(flightCode);
+                root->children.insert_or_assign(name, flightCodes);
+            }
+            else
+            {
+                root->children.at(name).push_back(flightCode);
+            }
+        }
     }
 
     std::vector<std::vector<std::string>> allPath;
